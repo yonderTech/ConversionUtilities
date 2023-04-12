7a14d701bcaf242a788595e0354bfc241d78d784
francisducharme <francis.ducharme@smartpixel.tv>
2023-04-11 20:46:46 -0400

    add unit test

---
+++ EntityMapperService.Tests/EntityMapperServiceTests.cs
@@ -0,0 +1,90 @@
+using Xunit;
+using Model = EntityMapperService.Tests.Model;
+using Dto = EntityMapperService.Tests.Dto;
+using Newtonsoft.Json;
+using ConversionUtilities;
+
+namespace EntityMapperService.Tests
+{
+    public class EntityMapperServiceTests
+    {
+        [Fact]
+        public void MainTest()
+        {
+            try
+            {
+                Model.A a = new Model.A()
+                {
+                    Id = Guid.NewGuid(),
+                    Id2 = Guid.NewGuid(),
+                    Prop1 = "Allo",
+                    Bs = new List<Model.B>
+                  {
+                      new Model.B
+                      {
+                           Name = "B1",
+                           C = new Model.C
+                           {
+                                ThingType = Model.ThingType.Hard,
+                                 Label = "C1"
+                           },
+                           As = new List<Model.A>
+                           {
+                              new Model.A
+                              {
+                                   Prop1 = "asdad",
+                                    Bs = new List<Model.B>
+                                    {
+                                        new Model.B
+                                        {
+                                             Name = "B1-2"
+                                        }
+                                    }
+                              }
+                           }
+                      },
+                      new Model.B
+                      {
+                           Name = "B2",
+                           C = new Model.C
+                           {
+                                ThingType = Model.ThingType.Cold,
+                                 Label = "C2"
+                           },
+                           As = new List<Model.A>
+                           {
+                              new Model.A
+                              {
+                                   Prop1 = "67567757",
+                                    Bs = new List<Model.B>
+                                    {
+                                        new Model.B
+                                        {
+                                             Name = "B2-2"
+                                        }
+                                    }
+                              }
+                           }
+                      }
+                  }
+                };
+
+                EntityMappingService entityMappingService = new EntityMappingService();
+
+                Dto.A converted = entityMappingService.Clone<Dto.A>(a);
+
+                bool nameSame = a.Prop1 == converted.Prop1;
+                bool IdSame = a.Id == converted.Id;
+                bool Id2Same = a.Id2 == converted.Id2;
+                bool Id3Same = a.Id3 == converted.Id3;
+                bool listSame = JsonConvert.SerializeObject(a.Bs) == JsonConvert.SerializeObject(converted.Bs);
+
+                Assert.True(nameSame && IdSame && Id2Same && Id3Same && listSame);
+            }
+            catch
+            {
+                Assert.True(false);
+            }
+        }
+    }
+}
---
+++ EntityMapperService.Tests/Objects/Dto.cs
@@ -0,0 +1,35 @@
+﻿namespace EntityMapperService.Tests.Dto
+{
+    public enum ThingType
+    {
+        Hard,
+        Cold,
+        Hot
+    }
+
+    public class A
+    {
+        public string Prop1 { get; set; }
+        public Guid Id { get; set; }
+        public Guid Id2 { get; set; }
+        public Guid Id3 { get; set; }
+        public List<B> Bs { get; set; } = new List<B>();
+    }
+
+
+    public class B
+    {
+        public string Name { get; set; }
+        public C C { get; set; }
+        public List<A> As { get; set; }
+    }
+
+
+
+    public class C
+    {
+        public string Label { get; set; }
+        public ThingType ThingType { get; set; }
+
+    }
+}
